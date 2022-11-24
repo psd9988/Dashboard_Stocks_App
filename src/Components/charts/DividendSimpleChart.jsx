dc067b53ff8c4fa01d52373c6ff27b08fab60d88
kssc90 <51017284+kssc90@users.noreply.github.com>
2022-11-24 16:10:25 +0530

    comments removed

--- src/Components/charts/DividendSimpleChart.jsx
+++ src/Components/charts/DividendSimpleChart.jsx
@@ -1,8 +1,5 @@
-/* DividendSimpleChart.js */
-// import React, { Component } from "react";
 import React from "react";
 import CanvasJSReact from "../canvasjs.stock.react";
-// var CanvasJS = CanvasJSReact.CanvasJS;
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 const DividendSimpleChart = () => {
@@ -32,34 +29,3 @@ const DividendSimpleChart = () => {
 
 export default DividendSimpleChart;
 
-// export default class DividendSimpleChart extends Component {
-// render() {
-// const options = {
-//   title: {
-//     text: "Monthly Dividend Chart",
-//   },
-//   data: [
-//     {
-//       // Change type to "doughnut", "line", "splineArea", etc.
-//       type: "column",
-//       dataPoints: [
-//         { label: "June", y: 10 },
-//         { label: "July", y: 15 },
-//         { label: "August", y: 25 },
-//         { label: "September", y: 30 },
-//         { label: "October", y: 28 },
-//       ],
-//     },
-//   ],
-// };
-// return (
-//   <div style={{height: "200px", width: "500px"}}>
-//     <CanvasJSChart
-//       options={options}
-//       /* onRef={ref => this.chart = ref} */
-//     />
-//     {/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-//   </div>
-// );
-// }
-// }
--- src/Components/charts/StockChart.jsx
+++ src/Components/charts/StockChart.jsx
@@ -26,8 +26,6 @@ const StockChart = ({ symbol }) => {
             parseInt(r[1]) - 1,
             parseInt(r[2])
           ).getTime();
-          // var newDate = date.getTime();
-          // console.log(value)
           dps.unshift({
             date: parseInt(dateInNumber),
             open: parseFloat(value["1. open"]),
@@ -115,12 +113,10 @@ const StockChart = ({ symbol }) => {
     <div>
       <div>
         {
-          // Reference: https://reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator
           isLoaded && (
             <CanvasJSStockChart
               containerProps={containerProps}
               options={options}
-              /* onRef = {ref => this.chart = ref} */
             />
           )
         }
@@ -129,100 +125,5 @@ const StockChart = ({ symbol }) => {
   );
 };
 
-// componentDidMount() {
-//   //Reference: https://reactjs.org/docs/faq-ajax.html#example-using-ajax-results-to-set-local-state
-//   fetch(
-//     "https://canvasjs.com/data/gallery/react/btcusd2017-18.json"
-//   )
-//     .then((res) => res.json())
-//     .then((data) => {
-//       var dps = [];
-//       for (var i = 0; i < data.length; i++) {
-//         dps.push({
-//           x: new Date(data[i].date),
-//           y: Number(data[i].close),
-//         });
-//       }
-//       this.setState({
-//         isLoaded: true,
-//         dataPoints: dps,
-//       });
-//     });
-// }
-
-// render() {
-//   const options = {
-//     title: {
-//       text: "React StockChart with Spline Area Chart",
-//     },
-//     theme: "light2",
-//     subtitles: [
-//       {
-//         text: "BTC/USD",
-//       },
-//     ],
-//     charts: [
-//       {
-//         axisX: {
-//           crosshair: {
-//             enabled: true,
-//             snapToDataPoint: true,
-//             valueFormatString: "MMM DD YYYY",
-//           },
-//         },
-//         axisY: {
-//           title: "Bitcoin Price",
-//           prefix: "$",
-//           crosshair: {
-//             enabled: true,
-//             snapToDataPoint: true,
-//             valueFormatString: "$#,###.##",
-//           },
-//         },
-//         toolTip: {
-//           shared: true,
-//         },
-//         data: [
-//           {
-//             name: "Price (in USD)",
-//             type: "splineArea",
-//             color: "#3576a8",
-//             yValueFormatString: "$#,###.##",
-//             xValueFormatString: "MMM DD YYYY",
-//             dataPoints: this.state.dataPoints,
-//           },
-//         ],
-//       },
-//     ],
-//     navigator: {
-//       slider: {
-//         minimum: new Date("2017-05-01"),
-//         maximum: new Date("2018-05-01"),
-//       },
-//     },
-//   };
-//   const containerProps = {
-//     width: "100%",
-//     height: "450px",
-//     margin: "auto",
-//   };
-//   return (
-//     <div>
-//       <div>
-//         {
-//           // Reference: https://reactjs.org/docs/conditional-rendering.html#inline-if-with-logical--operator
-//           this.state.isLoaded && (
-//             <CanvasJSStockChart
-//               containerProps={containerProps}
-//               options={options}
-//               /* onRef = {ref => this.chart = ref} */
-//             />
-//           )
-//         }
-//       </div>
-//     </div>
-//   );
-// }
-// }
 
 export default StockChart;
